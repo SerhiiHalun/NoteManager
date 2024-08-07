acffc236feb4e7663c7c5846f1b1bbf1e10fa1ba
SerhiiHalun <sergey.galun15@gmail.com>
2024-08-07 12:09:32 +0300

    Revert "Error: cannot find symbol"
    This reverts commit 7df1298e2b2fa0a4ffb5dacde814414b93513a6a.

--- src/main/java/com/example/NoteManager/NoteManagerApplication.java
+++ src/main/java/com/example/NoteManager/NoteManagerApplication.java
@@ -1,9 +1,7 @@
 package com.example.NoteManager;
 
-
-
-import com.example.NoteManager.entity.Note;
-import com.example.NoteManager.service.NoteService;
+import com.example.NoteManager.Entity.Note;
+import com.example.NoteManager.Service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
